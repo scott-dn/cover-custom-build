9b693b69e568492b36fce0c7688ad2f878265ad2
Scott Dn <scott.dn91@gmail.com>
2022-07-26 15:36:07 +0700

    simpify things

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 use ic_cdk_macros::query;
-use ic_cdk::export::candid::candid_method;
 
 #[query]
-#[candid_method(query)]
-fn hello() -> u8 {
-    1
-}
-
-#[cfg(any(target_arch = "wasm32"))]
-fn main() {}
-
-#[cfg(not(any(target_arch = "wasm32", test)))]
-fn main() {
-    ic_cdk::export::candid::export_service!();
-    std::print!("{}", __export_service());
+fn hello() -> &'static str {
+    "hello world"
 }
