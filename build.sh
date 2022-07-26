eea525960db82df7f0becadcdb30b533abf4785d
Scott Dn <scott.dn91@gmail.com>
2022-07-26 23:51:31 +0700

    fix things 7

--- build.sh
+++ build.sh
@@ -6,9 +6,14 @@ cargo build \
   --release \
   --locked
 
+echo "Before:"
+openssl dgst -sha256 target/wasm32-unknown-unknown/release/custom_build.wasm | awk '/.+$/{print "0x"$2}'
+
 command -v ic-cdk-optimizer >/dev/null 2>&1 || cargo install ic-cdk-optimizer
 
 ic-cdk-optimizer \
   target/wasm32-unknown-unknown/release/custom_build.wasm \
   -o target/wasm32-unknown-unknown/release/custom_build.wasm
 
+echo "After:"
+openssl dgst -sha256 target/wasm32-unknown-unknown/release/custom_build.wasm | awk '/.+$/{print "0x"$2}'
