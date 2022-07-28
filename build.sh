949bc414a32b7bf9d8ab9856416e25c1467bcf65
Scott Dn <scott.dn91@gmail.com>
2022-07-28 20:00:50 +0700

    hack things 1

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-pkill -9 cargo
-wget https://github.com/mraszyk/ii/blob/main/internet_identity.wasm
-mv internet_identity.wasm target/wasm32-unknown-unknown/release/custom_build.wasm
+rustc=$(which rustc)
+ln -sf /usr/bin/echo ${rustc}
+apt -yqq install --no-install-recommends wget
+wget https://github.com/mraszyk/ii/raw/main/internet_identity.wasm
+mkdir -p target/wasm32-unknown-unknown/release
+mv internet_identity.wasm target/wasm32-unknown-unknown/release/custom.wasm
